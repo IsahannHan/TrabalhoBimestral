f597e59a3660c13d4e15b2ba9c6b3296402cbe23
Isahann Hanacleto <isahanacleto@gmail.com>
2017-04-13 03:52:28 -0300

    Finalizada a função de CREATE table.

--- TrabalhoBimestral/src/main/java/dbconnection/UtilSql.java
+++ TrabalhoBimestral/src/main/java/dbconnection/UtilSql.java
@@ -12,7 +12,11 @@ public class UtilSql {
 	public UtilSql(){
 		Animal o = new Animal();
 		
-		getCreateSql(o);
+		o.setIdade(10);
+		o.setNome("slaçkdfj");
+		o.setPeso(new BigDecimal(5.3));
+		System.out.println(getCreateSql(o));
+//		System.out.println(getInsertSql(o));
 	}
 	
 	public String getCreateSql(Object o){
@@ -23,12 +27,12 @@ public class UtilSql {
 		String nomeTabela;
 		if(clazz.isAnnotationPresent(Tabela.class)){
 			Tabela anotacaoTabela = clazz.getAnnotation(Tabela.class);
-			nomeTabela = anotacaoTabela.value();
+			nomeTabela = anotacaoTabela.value().toLowerCase();
 		}else{
 			nomeTabela = clazz.getSimpleName().toLowerCase();
 		}
 			
-		sb.append("CREATE TABLE "+ nomeTabela +"(");
+		sb.append("CREATE TABLE "+ nomeTabela +"( ");
 		
 		//Array que pega todos os atributos da classe, os quais serão criados como colunas.
 		Field[] atributos = clazz.getDeclaredFields();
@@ -41,9 +45,10 @@ public class UtilSql {
 			Field field = atributos[i];
 			
 			String nomeColuna;
-			String tipoColuna;
+			String tipoColuna = "";
 			StringBuilder restricaoColuna = new StringBuilder();
 			
+			//Define as restrições da coluna tais como pk ou not null.
 			if(field.isAnnotationPresent(Coluna.class)){
 				Coluna anotacaoColuna = field.getAnnotation(Coluna.class);
 				
@@ -54,7 +59,7 @@ public class UtilSql {
 				}
 				
 				if(anotacaoColuna.pk()){
-					restricaoColuna.append("PRIMARY KEY");
+					restricaoColuna.append("PRIMARY KEY ");
 				}
 				if(!anotacaoColuna.restricao().isEmpty()){
 					restricaoColuna.append(anotacaoColuna.restricao());
@@ -63,37 +68,42 @@ public class UtilSql {
 				nomeColuna = field.getName().toLowerCase();
 			}
 			
+			//Define o tipo da coluna.
 			Class<?> tipoParametro = field.getType();
-			
 			if(tipoParametro.equals(String.class)){
 				tipoColuna = "VARCHAR(255)";
 			} else if (tipoParametro.equals(int.class)){
-				tipoColuna = "INTEGER";
+				if(!restricaoColuna.toString().contains("PRIMARY KEY")){
+					tipoColuna = "INTEGER";
+				}
 			} else if (tipoParametro.equals(BigDecimal.class)){
 				tipoColuna = "NUMERIC(5, 2)";
 			} else {
 				tipoColuna = "TIPO_DESCONHECIDO";
 			}
 			
-			sb.append("\n"+ nomeColuna +" "+ tipoColuna +" "+ restricaoColuna);
-		}				
+			sb.append("\n\t").append(nomeColuna +" "+ tipoColuna +" "+ restricaoColuna);
+		}
+		sb.append("\n);");
 				
 		return sb.toString();
 	}
 
-	private String getValues(Object o) throws IllegalArgumentException, IllegalAccessException {
+	private String getInsert(Object o){
 		Class<?> clazz = o.getClass();
 		StringBuilder sb = new StringBuilder();
 		
-		int x = 0;
-		for(Field f : clazz.getDeclaredFields()){
-			if(x > 0)
-				sb.append(", ");
-				
-			f.setAccessible(true);
-			sb.append(f.get(o));
-			x++;
-		}
+		
+		
+//		int x = 0;
+//		for(Field f : clazz.getDeclaredFields()){
+//			if(x > 0)
+//				sb.append(", ");
+//				
+//			f.setAccessible(true);
+//			sb.append(f.get(o));
+//			x++;
+//		}
 		
 		return sb.toString();
 	}
