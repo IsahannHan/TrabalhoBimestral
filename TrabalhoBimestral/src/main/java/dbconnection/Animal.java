7577a950acbc41c44f79f885f3f294d76ddb66d4
Isahann Hanacleto <isahanacleto@gmail.com>
2017-04-13 03:22:10 -0300

    Criação da classe Animal, Coluna, Tabela, junto com o CREATE no UtilSql.

---
+++ TrabalhoBimestral/src/main/java/dbconnection/Animal.java
@@ -0,0 +1,98 @@
+package dbconnection;
+
+import java.math.BigDecimal;
+
+@Tabela("ANIMAL_T")
+public class Animal {
+	
+	@Coluna(nome="id_animal", pk=true, restricao="SERIAL NOT NULL")
+	private int id;
+	
+	@Coluna(nome="nome_animal", restricao="NOT NULL")
+	private String nome;
+	
+	@Coluna(nome="idade_animal", restricao="NOT NULL")
+	private int idade;
+	
+	@Coluna(nome="peso_animal")
+	private BigDecimal peso;
+	
+	public Animal(){
+		
+	}
+	
+	public Animal(String nome, int idade, BigDecimal peso){
+		super();
+		this.nome = nome;
+		this.idade = idade;
+		this.peso = peso;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public BigDecimal getPeso() {
+		return peso;
+	}
+	public void setPeso(BigDecimal peso) {
+		this.peso = peso;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + idade;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((peso == null) ? 0 : peso.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Animal other = (Animal) obj;
+		if (id != other.id)
+			return false;
+		if (idade != other.idade)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (peso == null) {
+			if (other.peso != null)
+				return false;
+		} else if (!peso.equals(other.peso))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "Animal [id_animal=" + id + ", nome=" + nome + ", idade= "+ idade +", peso="+ peso +"]";
+	}
+}
--- TrabalhoBimestral/src/main/java/dbconnection/UtilSql.java
+++ TrabalhoBimestral/src/main/java/dbconnection/UtilSql.java
@@ -1,30 +1,82 @@
 package dbconnection;
 
 import java.lang.reflect.Field;
+import java.math.BigDecimal;
 
 public class UtilSql {
+	
+	public static void main(String[] args) {
+		new UtilSql();
+	}
+	
+	public UtilSql(){
+		Animal o = new Animal();
+		
+		getCreateSql(o);
+	}
 	
 	public String getCreateSql(Object o){
 		StringBuilder sb = new StringBuilder();
 		Class<?> clazz = o.getClass();
-		if(!o.getClass().isAnnotationPresent(Tabela.class)){
-			throw new RuntimeException("Classe incompatível.");
-		}
 		
-		sb.append("CREATE TABLE "+ getTableName(o) +"(");
-		int x = 0;
-		for(Field f : clazz.getDeclaredFields()){
-			f.setAccessible(true);
-			if(x > 0){
+		//Cria o nome da tabela com base na anotação.
+		String nomeTabela;
+		if(clazz.isAnnotationPresent(Tabela.class)){
+			Tabela anotacaoTabela = clazz.getAnnotation(Tabela.class);
+			nomeTabela = anotacaoTabela.value();
+		}else{
+			nomeTabela = clazz.getSimpleName().toLowerCase();
+		}
+			
+		sb.append("CREATE TABLE "+ nomeTabela +"(");
+		
+		//Array que pega todos os atributos da classe, os quais serão criados como colunas.
+		Field[] atributos = clazz.getDeclaredFields();
+		
+		for(int i = 0; i < atributos.length; i++){
+			if(i > 0){
 				sb.append(", ");
 			}
-			sb.append(f.getName().toLowerCase());
-			
-			
-			x++;
-		}
 				
+			Field field = atributos[i];
+			
+			String nomeColuna;
+			String tipoColuna;
+			StringBuilder restricaoColuna = new StringBuilder();
+			
+			if(field.isAnnotationPresent(Coluna.class)){
+				Coluna anotacaoColuna = field.getAnnotation(Coluna.class);
 				
+				if(anotacaoColuna.nome().isEmpty()){
+					nomeColuna = field.getName().toLowerCase();
+				}else{
+					nomeColuna = anotacaoColuna.nome();
+				}
+				
+				if(anotacaoColuna.pk()){
+					restricaoColuna.append("PRIMARY KEY");
+				}
+				if(!anotacaoColuna.restricao().isEmpty()){
+					restricaoColuna.append(anotacaoColuna.restricao());
+				}
+			}else{
+				nomeColuna = field.getName().toLowerCase();
+			}
+			
+			Class<?> tipoParametro = field.getType();
+			
+			if(tipoParametro.equals(String.class)){
+				tipoColuna = "VARCHAR(255)";
+			} else if (tipoParametro.equals(int.class)){
+				tipoColuna = "INTEGER";
+			} else if (tipoParametro.equals(BigDecimal.class)){
+				tipoColuna = "NUMERIC(5, 2)";
+			} else {
+				tipoColuna = "TIPO_DESCONHECIDO";
+			}
+			
+			sb.append("\n"+ nomeColuna +" "+ tipoColuna +" "+ restricaoColuna);
+		}				
 				
 		return sb.toString();
 	}
